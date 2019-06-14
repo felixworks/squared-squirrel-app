2faa6eb38ee0e9335fd610cd1e5efa3dcf50153f
Felix Meyer <phelixm@gmail.com>
2019-06-13 23:14:26 -0500

    add winning tile, hardcode enemies for now

--- src/GameTile.js
+++ src/GameTile.js
@@ -2,13 +2,14 @@ import React from "react";
 
 function GameTile(props) {
   // randomly select some tiles to have enemies
-  let tileType = "grid-item";
-  if (
-    props.xValue === Math.floor(Math.random() * 10) &&
-    props.yValue === Math.floor(Math.random() * 10)
-  ) {
-    tileType = "grid-item enemy-tile";
+  let tileType = "grid-item ";
+  if (props.hasEnemy) {
+    tileType = tileType + "enemy-tile";
   }
+  if (props.hasWinningTile) {
+    tileType = tileType + "winning-tile";
+  }
+
   return (
     <div className={tileType}>
       Square <br /> x={props.xValue} y={props.yValue} <br />
--- src/Gameboard.js
+++ src/Gameboard.js
@@ -4,8 +4,14 @@ import Player from "./Player";
 
 class Gameboard extends React.Component {
   state = {
-    playerX: 9,
-    playerY: 9
+    player: {
+      playerX: 9,
+      playerY: 9
+    },
+
+    enemy1: null,
+    enemy2: null,
+    enemy3: null
   };
 
   handleKeyDown = e => {
@@ -13,44 +19,52 @@ class Gameboard extends React.Component {
     const keyPressedDown = e.code;
     switch (keyPressedDown) {
       case "ArrowUp":
-        if (this.state.playerY === 0) {
-          console.log("Game barrier reached");
+        if (this.state.player.playerY === 0) {
           return null;
         }
         this.setState({
-          playerY: this.state.playerY - 1
+          player: {
+            ...this.state.player,
+            playerY: this.state.player.playerY - 1
+          }
         });
     }
     switch (keyPressedDown) {
       case "ArrowDown":
-        if (this.state.playerY === 9) {
-          console.log("Game barrier reached");
+        if (this.state.player.playerY === 9) {
           return null;
         }
         this.setState({
-          playerY: this.state.playerY + 1
+          player: {
+            ...this.state.player,
+            playerY: this.state.player.playerY + 1
+          }
         });
     }
 
     switch (keyPressedDown) {
       case "ArrowRight":
-        if (this.state.playerX === 9) {
-          console.log("Game barrier reached");
+        if (this.state.player.playerX === 9) {
           return null;
         }
         this.setState({
-          playerX: this.state.playerX + 1
+          player: {
+            ...this.state.player,
+            playerX: this.state.player.playerX + 1
+          }
         });
     }
 
     switch (keyPressedDown) {
       case "ArrowLeft":
-        if (this.state.playerX === 0) {
-          console.log("Game barrier reached");
+        if (this.state.player.playerX === 0) {
           return null;
         }
         this.setState({
-          playerX: this.state.playerX - 1
+          player: {
+            ...this.state.player,
+            playerX: this.state.player.playerX - 1
+          }
         });
     }
   };
@@ -59,15 +73,51 @@ class Gameboard extends React.Component {
     window.addEventListener("keydown", this.handleKeyDown);
   }
 
+  // randomNumberGenerator = () => {
+  //   return Math.floor(Math.random() * 10);
+  // };
+
+  // enemyGeneration = () => {
+  //   const amountOfEnemies = 6;
+  //   let randomNumbers = [];
+  //   while (randomNumbers.length < amountOfEnemies) {
+  //     const randomNumber = Math.floor(Math.random() * 10);
+  //     randomNumbers.push(randomNumber);
+  //   }
+  //   this.setState({
+  //     enemy1: { x: randomNumbers[0], y: randomNumbers[1] },
+  //     enemy2: { x: randomNumbers[2], y: randomNumbers[3] },
+  //     enemy3: { x: randomNumbers[4], y: randomNumbers[5] }
+  //   });
+  // };
+
   // if performance issues arise, refactor this loop
   generateGameTileArray = () => {
     let gameTileArray = [];
     for (let y = 0; y < 10; y++) {
       for (let x = 0; x < 10; x++) {
-        let hasPlayer = x === this.state.playerX && y === this.state.playerY;
+        // checks to see whether the player location in state matches the current tile being generated
+        let hasPlayer =
+          x === this.state.player.playerX && y === this.state.player.playerY;
+
+        // sets hasEnemy property on various tiles
+        const enemyCoordinates = [[1, 3], [4, 5], [7, 8]];
+        let hasEnemy =
+          (x === enemyCoordinates[0][0] && y === enemyCoordinates[0][1]) ||
+          (x === enemyCoordinates[1][0] && y === enemyCoordinates[1][1]) ||
+          (x === enemyCoordinates[2][0] && y === enemyCoordinates[2][1]);
+
+        const winningTile = [0, 0];
+        let hasWinningTile = x === winningTile[0] && y === winningTile[1];
 
         gameTileArray.push(
-          <GameTile xValue={x} yValue={y} key={`${x},${y}`}>
+          <GameTile
+            xValue={x}
+            yValue={y}
+            key={`${x},${y}`}
+            hasEnemy={hasEnemy}
+            hasWinningTile={hasWinningTile}
+          >
             {hasPlayer && <Player />}
           </GameTile>
         );
@@ -75,6 +125,7 @@ class Gameboard extends React.Component {
     }
     return gameTileArray;
   };
+
   render() {
     return (
       <section className="grid-container">
