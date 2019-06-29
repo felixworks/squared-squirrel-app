70fe55ec2cf2557ff5d51ce2d4394d53637eaddd
Felix Meyer <phelixm@gmail.com>
2019-06-29 13:08:16 -0500

    API tweak

--- src/config.js
+++ src/config.js
@@ -1,6 +1,5 @@
 module.exports = {
   PORT: process.env.PORT || 8080,
-  API_BASE_URL:
-    // "https://serene-beach-22116.herokuapp.com/api"
-    "http://localhost:8000/api"
+  API_BASE_URL: "https://serene-beach-22116.herokuapp.com/api"
+  // "http://localhost:8000/api"
 };
