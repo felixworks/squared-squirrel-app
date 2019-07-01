50599544a54983e727ea2ab6cfbce9f558e35883
Felix Meyer <phelixm@gmail.com>
2019-07-01 17:04:21 -0500

    CORS change

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 module.exports = {
   PORT: process.env.PORT || 8080,
-  API_BASE_URL: "http://localhost:8000/api"
-  // "https://serene-beach-22116.herokuapp.com/api"
+  API_BASE_URL: "https://serene-beach-22116.herokuapp.com/api"
+  // "http://localhost:8000/api"
 };
